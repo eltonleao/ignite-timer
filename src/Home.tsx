585da1b10ff07cda83c91acae70a168b642fd3cc
Elton Leão <elton.leao@peopleinteractive.com.br>
2024-01-13 13:27:57 -0300

    styled components

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,43 +1,13 @@
-import { createContext, useContext, useState } from "react";
+import { Button } from "./components/Button";
 
-interface HomeContextData {
-  teste: string;
-  setTeste: (teste: string) => void;
-}
-
-const HomeContext = createContext({} as HomeContextData);
-
-function ComponentA() {
-  const { teste, setTeste } = useContext(HomeContext);
-  return (
-    <div>
-      <h1>Component A: {teste}</h1>
-      <button
-        onClick={() => {
-          setTeste("kajdshksajd");
-        }}
-      >
-        click
-      </button>
-    </div>
-  );
-}
-
-function ComponentB() {
-  const { teste } = useContext(HomeContext);
-  return (
-    <div>
-      <h1>Component B: {teste}</h1>
-    </div>
-  );
-}
 
 export function Home() {
-  const [teste, setTeste] = useState("teste");
   return (
-    <HomeContext.Provider value={{ teste, setTeste }}>
-      <ComponentA />
-      <ComponentB />
-    </HomeContext.Provider>
+    <>
+    <Button variant="primary"/>
+    <Button variant="primary"/>
+    <Button variant="secondary"/>
+    <Button variant="secondary"/>
+    </>
   );
 }
